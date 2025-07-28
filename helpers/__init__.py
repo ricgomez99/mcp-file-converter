208f198a002084f906ef58e5d876bb2d5bba610d
Andres <argomez.restrepo@gmail.com>
2025-07-28 18:59:38 -0500

    tool failed

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -2,5 +2,12 @@ from .pdf_to_docx import pdf_to_docx
 from .files_list import FilesList
 from .convert_pdf import ConvertPdf
 from .docx_to_pdf import docx_to_pdf
+from .convert_docx import ConvertToPdf
 
-__all__ = ['pdf_to_docx', 'FilesList', 'ConvertPdf', 'docx_to_pdf']
+__all__ = [
+    'pdf_to_docx', 
+    'FilesList', 
+    'ConvertPdf', 
+    'docx_to_pdf', 
+    'ConvertToPdf'
+    ]
--- tools/__init__.py
+++ tools/__init__.py
@@ -1,3 +1,4 @@
 from .pdf_to_docx_tool import pdf_to_docx_tool
+from .docx_to_pdf_tool import docx_to_pdf_tool
 
-__all__ = ['pdf_to_docx_tool']
+__all__ = ['pdf_to_docx_tool', 'docx_to_pdf_tool']
